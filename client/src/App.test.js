834a2ead1e699563694785103d2b5db17ff3829d
Alexander Belyaev <alexanderbelyaevmacbookair@alexanders-air.lan>
2022-08-16 10:53:23 -0700

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+it('renders without crashing', () => {});
